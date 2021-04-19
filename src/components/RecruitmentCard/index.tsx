c396af771ef4576adbd867de89a1fa46a50b0b8a
westlag114 <fsww2353@gmail.com>
2021-04-19 10:15:40 +0900

    レビューテスト用プルリク1

--- src/components/RecruitmentCard/index.tsx
+++ src/components/RecruitmentCard/index.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./style.module.scss";
+import image from "../../images/Rectangle77.png";
+import Avatar from "../Avatar/index";
 
 const RecruitmentCard = () => {
-  return <div className={styles.card}>recruitmentCard</div>;
+  return (
+    <>
+      <div className={styles.card}>
+        <div>
+          <img src={image} className={styles.image} />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.title}>
+            三度の飯よりReact!フロントエンドエキスパートになりたいエンジニア募集
+          </p>
+          <div className={styles.company}>
+            <Avatar />
+            <div className={styles.name}>
+              <p>株式会社</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RecruitmentCard;
